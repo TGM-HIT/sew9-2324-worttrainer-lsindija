d2ceeb5f50fba829fde794712e8022ce51250e2d
lsindija <lsindija@student.tgm.ac.at>
2023-09-22 11:44:19 +0200

    Main Klasse

--- src/main/java/lsindija/Main.java
+++ src/main/java/lsindija/Main.java
@@ -1,7 +1,24 @@
 package lsindija;
 
+import lsindija.gui.WorttrainerFrame;
+import lsindija.model.Rechtschreibtrainer;
+import lsindija.model.Wort;
+import lsindija.persistenz.Persistence;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        Persistence persistence = new Persistence();
+        Rechtschreibtrainer trainer = persistence.load();
+        trainer.auswaehlen();
+
+        String[] options = {"Abschicken", "Beenden"};
+        boolean beendet = false;
+
+        WorttrainerFrame frame = new WorttrainerFrame(trainer);
+        while(!beendet) {
+            frame.setImage(trainer.getAusgewaehlt().getImage());
+        }
     }
 }
