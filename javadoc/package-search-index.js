c55276faf307cb052f64d112629de4bdd895e61f
lsindija <95346563+lsindija@users.noreply.github.com>
2023-09-22 09:54:23 +0000

    Deploying to javadoc from @ TGM-HIT/sew9-2324-worttrainer-lsindija@83b06ae95e0d889f4632f64cec006d5b518f1d85 🚀

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"lsindija"},{"l":"lsindija.gui"},{"l":"lsindija.model"},{"l":"lsindija.persistenz"}];updateSearchResults();
